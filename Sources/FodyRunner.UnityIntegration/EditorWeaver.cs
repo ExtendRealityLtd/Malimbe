3205065e720f3f01dbd96896661b8f7e5a3d67be
Christopher-Marcel Böddecker <bddckr@icloud.com>
2019-01-05 14:12:55 +0100

    fix(UnityIntegration): Unity doesn't pick up changes
    Hooking into Unity's assembly reloading mechanism and preventing the
    reloading while the assemblies are processed doesn't work without
    reloading the assemblies afterwards, otherwise Unity won't notice
    the changes done to the assemblies. This fix changes the approach
    completely to instead hook into Unity's compilation pipeline and
    post-processes the assembly synchronously before Unity will then
    (re)load that changed assembly. While this adds a few seconds of
    processing while the Unity Editor is blocked this tradeoff is
    similar to the fix of the previous approach as that one would have
    to reload all assemblies, potentially even taking longer in projects
    that use Assembly Definition Files and managed .dlls (as those will
    only be processed once by this fix).

--- Sources/FodyRunner.UnityIntegration/EditorWeaver.cs
+++ Sources/FodyRunner.UnityIntegration/EditorWeaver.cs
@@ -2,133 +2,48 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
-    using System.Reflection;
-    using System.Threading;
-    using System.Threading.Tasks;
     using UnityEditor;
     using UnityEditor.Compilation;
     using UnityEngine;
-    using Assembly = UnityEditor.Compilation.Assembly;
 
     internal static class EditorWeaver
     {
-        private static readonly string _lastWeaveTimeTicksKey =
-            $"{typeof(EditorWeaver).FullName}.{nameof(_lastWeaveTimeTicksKey)}";
-        private static CancellationTokenSource _cancellationTokenSource;
+        private static readonly Runner _runner = new Runner(new Logger());
 
         [InitializeOnLoadMethod]
-        private static void OnEditorInitialization()
+        private static void OnEditorInitialization() =>
+            CompilationPipeline.assemblyCompilationFinished += (path, messages) => WeaveAssembly(path);
+
+        private static void WeaveAssembly(string assemblyFilePath)
         {
-            WeaveEditorAssemblies();
-
-            AssemblyReloadEvents.beforeAssemblyReload += () =>
+            Assembly foundAssembly = CompilationPipeline.GetAssemblies(AssembliesType.Player)
+                .Concat(CompilationPipeline.GetAssemblies(AssembliesType.Editor))
+                .FirstOrDefault(
+                    assembly => string.Equals(assembly.outputPath, assemblyFilePath, StringComparison.Ordinal));
+            if (foundAssembly == null)
             {
-                long lastWriteTimeTicks = File.GetLastWriteTimeUtc(typeof(EditorWeaver).Assembly.Location).Ticks;
-                if (long.Parse(SessionState.GetString(_lastWeaveTimeTicksKey, "-1")) <= lastWriteTimeTicks)
-                {
-                    /*
-                     * The assembly this type is in changed which means Unity will reload this assembly and call this method again.
-                     * Thus we shouldn't weave with the previous assembly that is executing at this point.
-                     */
-                    return;
-                }
-
-                WeaveEditorAssemblies();
-            };
-
-            ExecuteSynchronizationContextIfNeeded();
-        }
-
-        private static void WeaveEditorAssemblies()
-        {
-            _cancellationTokenSource?.Cancel();
-            _cancellationTokenSource?.Dispose();
-
-            SessionState.SetString(_lastWeaveTimeTicksKey, DateTime.UtcNow.Ticks.ToString());
-            EditorApplication.LockReloadAssemblies();
-            Assembly[] assemblies = CompilationPipeline.GetAssemblies(AssembliesType.Player);
-
-            if (assemblies.Length == 0)
-            {
-                EditorApplication.UnlockReloadAssemblies();
                 return;
             }
 
-            _cancellationTokenSource = new CancellationTokenSource();
-            CancellationToken cancellationToken = _cancellationTokenSource.Token;
-
-            Task.Run(
-                    async () =>
-                    {
-                        try
-                        {
-                            Runner runner = new Runner(new Logger());
-
-                            foreach (Assembly assembly in assemblies)
-                            {
-                                if (_cancellationTokenSource.IsCancellationRequested)
-                                {
-                                    break;
-                                }
-
-                                string assemblyPath =
-                                    WeaverPathsHelper.AddProjectPathRootIfNeeded(assembly.outputPath);
-                                IEnumerable<string> references =
-                                    assembly.allReferences.Select(WeaverPathsHelper.AddProjectPathRootIfNeeded);
-
-                                await runner.RunAsync(
-                                        WeaverPathsHelper.SearchPaths,
-                                        assemblyPath,
-                                        references,
-                                        assembly.defines.ToList(),
-                                        WeaverPathsHelper.SearchPaths,
-                                        true,
-                                        cancellationToken)
-                                    .ConfigureAwait(false);
-                            }
-                        }
-                        catch (Exception exception)
-                        {
-                            Debug.LogException(exception);
-                        }
-                    },
-                    cancellationToken)
-                .ContinueWith(
-                    _ => EditorApplication.UnlockReloadAssemblies(),
-                    CancellationToken.None,
-                    TaskContinuationOptions.DenyChildAttach | TaskContinuationOptions.ExecuteSynchronously,
-                    TaskScheduler.FromCurrentSynchronizationContext());
-        }
-
-        private static void ExecuteSynchronizationContextIfNeeded()
-        {
-            // Task continuations are not run in the Editor before Unity 2018.2.0.
-            string[] versionParts = Application.unityVersion.Split('.');
-            if (versionParts.Length == 0
-                || !Version.TryParse(string.Join(".", versionParts, 0, versionParts.Length - 1), out Version version))
+            try
             {
-                Debug.LogWarning(
-                    $"Unable to parse the Unity version from the string '{Application.unityVersion}' reported by Unity."
-                    + " A workaround for task continuations not working in the Editor before 2018.2.0 won't be applied.");
-                return;
+                string assemblyPath = WeaverPathsHelper.AddProjectPathRootIfNeeded(foundAssembly.outputPath);
+                IEnumerable<string> references =
+                    foundAssembly.allReferences.Select(WeaverPathsHelper.AddProjectPathRootIfNeeded);
+                _runner.RunAsync(
+                        WeaverPathsHelper.SearchPaths,
+                        assemblyPath,
+                        references,
+                        foundAssembly.defines.ToList(),
+                        WeaverPathsHelper.SearchPaths,
+                        true)
+                    .GetAwaiter()
+                    .GetResult();
             }
-
-            if (version < new Version(2018, 2))
+            catch (Exception exception)
             {
-                EditorApplication.update += () =>
-                {
-                    if (EditorApplication.isPlayingOrWillChangePlaymode)
-                    {
-                        return;
-                    }
-
-                    SynchronizationContext context = SynchronizationContext.Current;
-                    context.GetType()
-                        .GetMethod("Exec", BindingFlags.NonPublic | BindingFlags.Instance)
-                        ?.Invoke(context, null);
-                };
+                Debug.LogException(exception);
             }
         }
     }
