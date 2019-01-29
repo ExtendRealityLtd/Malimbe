ba8d4acdebc529368bbe264fe7577f560eec7d42
Christopher-Marcel Böddecker <bddckr@icloud.com>
2019-01-29 11:54:09 +0100

    feat(PropertySetter): allow using property inside called method
    If the annotated method tried to call another method that uses the
    property it would end up working on the previous value instead of
    whatever this method returns. This change allows to set the value of
    the property's backing field at any time, thus it can be set before
    calling a method that needs the latest property value by calling the
    property's getter.
    
    BREAKING CHANGE: `SetsPropertyAttribute` was renamed to
    `CalledBySetterAttribute` and the expected signature of the
    annotated method changed. Please see the latest Readme for details.

--- Sources/PropertySetterMethod/CalledBySetterAttribute.cs
+++ Sources/PropertySetterMethod/CalledBySetterAttribute.cs
@@ -3,13 +3,13 @@
     using System;
 
     /// <summary>
-    /// Indicates that the method acts as a setter for a property.
+    /// Indicates that the method is called inside the setter of a property.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
-    public sealed class SetsPropertyAttribute : Attribute
+    public sealed class CalledBySetterAttribute : Attribute
     {
         /// <summary>
-        /// The name of the property this methods acts as a setter for.
+        /// The name of the property this method will be called from.
         /// </summary>
         /// <remarks>
         /// The property needs to be declared in the same type this attribute is used in and needs both a getter and setter of any accessibility.
@@ -19,10 +19,10 @@
         public readonly string PropertyName;
 
         /// <summary>
-        /// Indicates that the method acts as a setter for a property.
+        /// Indicates that the method is called inside the setter of a property.
         /// </summary>
-        /// <param name="propertyName">The name of the property this methods acts as a setter for.</param>
-        public SetsPropertyAttribute(string propertyName) =>
+        /// <param name="propertyName">The name of the property this method will be called from.</param>
+        public CalledBySetterAttribute(string propertyName) =>
             PropertyName = propertyName;
     }
 }
